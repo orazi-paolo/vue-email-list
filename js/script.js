413be3a6006cf74403fed4dc07da7bf1363fae4b
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-11 22:12:42 +0200

    added the logical part

--- js/script.js
+++ js/script.js
@@ -3,7 +3,35 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            // creo un dato per contenere le email
+            emails: [],
+            // creo un dato per contare le email
+            emailsCount: 0,
         }
+    },
+    methods: {
+        // genero 10 chiamate all'API per ottenere 10 email
+        getEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                        // verifico dove si trovano le email nell oggetto di risposta
+                        console.log(response);
+                        // pusho le email randomiche nell'array emails
+                        this.emails.push(response.data.response)
+                        // incremento il contatore delle email
+                        this.emailsCount++;
+                        // se ho ottenuto 10 email, stampo in console l'array emails
+                        if (this.emailsCount === 10) {
+                            console.log(this.emails);
+                        }
+                    })
+            }
+        },
+
+    },
+    created() {
+        // chiamo il metodo per ottenere le email
+        this.getEmails();
     }
 }).mount('#app')
